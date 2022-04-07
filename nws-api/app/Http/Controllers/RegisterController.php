c1cda175eb3f3ea5cd28262390e4270b45271d9d
azharjk <azhr.project@gmail.com>
2022-04-07 13:18:40 +0700

    Add `User Register` feature
    This patch include:
    - Create RegisterController
    - Create TokenResource
    - Alter users column email to username
    - etc

---
+++ nws-api/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Http\Resources\TokenResource;
+
+class RegisterController extends Controller
+{
+    public function index(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'username' => 'required|unique:users|min:8',
+            'password' => 'required|min:8'
+        ]);
+
+
+        if ($validator->fails()) {
+            return Response::make($validator->errors(), 400);
+        }
+
+        $validated = $validator->validated();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'username' => $validated['username'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        $token = $user->createToken('auth_token');
+
+        return new TokenResource($token);
+    }
+}
