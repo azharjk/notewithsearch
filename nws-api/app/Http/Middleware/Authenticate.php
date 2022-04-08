d03de2b70541655d0e5cb2d7129805a1f78b5a28
azharjk <azhr.project@gmail.com>
2022-04-08 17:08:01 +0700

    Modify the default response for unauthorized

--- nws-api/app/Http/Middleware/Authenticate.php
+++ nws-api/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,14 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Response;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    protected function unauthenticated($request, array $guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        $response = Response::json(['message' => 'Unauthorized'], 401);
+
+        abort($response);
     }
 }
