3fd3e15a376c64cbf86afb01bc4aee7b45045fe8
azharjk <azhr.project@gmail.com>
2022-04-15 02:09:00 +0700

    Remove logic from register component
    Just finish the UI first and then the logic

--- nws/src/app/register/register.component.ts
+++ nws/src/app/register/register.component.ts
@@ -1,46 +1,10 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements AfterViewInit {
+export class RegisterComponent {
 
-  @ViewChild('formControlName')
-  formControlName!: ElementRef<HTMLDivElement>;
-
-  @ViewChild('formControlUsername')
-  formControlUsername!: ElementRef<HTMLDivElement>;
-
-  @ViewChild('formControlPassword')
-  formControlPassword!: ElementRef<HTMLDivElement>;
-
-  formControls: ElementRef<HTMLDivElement>[] = [];
-
-  currentFormControl = 0;
-
-  constructor() { }
-
-  ngAfterViewInit(): void {
-    this.formControls = [
-      this.formControlName,
-      this.formControlUsername,
-      this.formControlPassword
-    ];
-  }
-
-  nextControl(): void {
-    if (this.currentFormControl + 1 >= this.formControls.length) {
-      return;
-    }
-
-    this.formControls[this.currentFormControl]
-      .nativeElement.classList.add('hidden');
-
-    this.formControls[this.currentFormControl + 1]
-      .nativeElement.classList.remove('hidden');
-
-    this.currentFormControl++;
-  }
 }
