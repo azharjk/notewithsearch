a6729f90090f978fc83e36e51e39305ae10f8c87
azharjk <azhr.project@gmail.com>
2022-04-14 15:12:22 +0700

    Add partial implementation of register page
    Only mobile screen and cannot procced any input

--- nws/src/app/register/register.component.ts
+++ nws/src/app/register/register.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements AfterViewInit {
+
+  @ViewChild('formControlName')
+  formControlName!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('formControlUsername')
+  formControlUsername!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('formControlPassword')
+  formControlPassword!: ElementRef<HTMLDivElement>;
+
+  formControls: ElementRef<HTMLDivElement>[] = [];
+
+  currentFormControl = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.formControls = [
+      this.formControlName,
+      this.formControlUsername,
+      this.formControlPassword
+    ];
   }
 
+  nextControl(): void {
+    if (this.currentFormControl + 1 >= this.formControls.length) {
+      return;
+    }
+
+    this.formControls[this.currentFormControl]
+      .nativeElement.classList.add('hidden');
+
+    this.formControls[this.currentFormControl + 1]
+      .nativeElement.classList.remove('hidden');
+
+    this.currentFormControl++;
+  }
 }
